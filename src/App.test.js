3721bc4dadf02cc4fdc8b229a473b2dabeab80e3
Oleh Myhal <adriancraciunel1@gmail.com>
2022-02-08 13:30:46 +0200

    test counter

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,55 @@
-import { render, screen } from '@testing-library/react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
-test('renders without crashing', () => {
-  const wrapper = shallow(<App />)
-  const appComponent = wrapper.find("[data-test='component-app']")
+const setup = () => shallow(<App />)
+
+const findByTestAttr = (wrapper, val) =>  wrapper.find(`[data-test='${val}']`)
+
+test('renders without crashing', async () => {
+  const wrapper = setup()
+  const appComponent = await findByTestAttr(wrapper, 'component-app')
   expect(appComponent.length).toBe(1)
 });
 
-test('renders increment button', () => {
-
+test('renders increment button', async () => {
+  const wrapper = setup()
+  const button = await findByTestAttr(wrapper, 'increment-button') 
+  expect(button.length).toBe(1)
 })
 
-test('renders counter display', () => {
-
+test('renders decrement button', async () => {
+  const wrapper = setup()
+  const button = await findByTestAttr(wrapper, 'decrement-button') 
+  expect(button.length).toBe(1)
 })
 
-test('renders display counter starts 0', () => {
-
+test('renders counter display', async () => {
+  const wrapper = setup()
+  const counterDisplay = await findByTestAttr(wrapper, 'counter-display')  
+  expect(counterDisplay.length).toBe(1)
 })
 
-test('clicking button increment counter display', () => {
+test('renders display counter starts at 0', async () => {
+  const wrapper = setup()
+  const counter = await findByTestAttr(wrapper, 'count')
+  expect(counter.text()).toBe('0')
+})
 
+test('clicking button increment counter display', async () => {
+  const wrapper = setup()
+  const button = await findByTestAttr(wrapper, 'increment-button') 
+  button.simulate('click')
+  const counterDisplay = await findByTestAttr(wrapper, 'count')
+  expect(counterDisplay.text()).toBe('1')
+})
+
+test('clicking button decrement counter display', async () => {
+  const wrapper = setup()
+  const button = await findByTestAttr(wrapper, 'decrement-button') 
+  button.simulate('click')
+  const counterDisplay = await findByTestAttr(wrapper, 'count')
+  expect(counterDisplay.text()).toBe('-1')
 })
